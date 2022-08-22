0c19abb7289d3603e66e3e9f69830e75882397c5
Juan <juanllama160@gmail.com>
2022-08-22 20:31:24 -0300

    ultimo commit

--- Front-end/Portfolio/src/environments/environment.prod.ts
+++ Front-end/Portfolio/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDok89ziI3rr2W4hlAsrCifl26YSTlNTQc",
+    authDomain: "portfolio-e0307.firebaseapp.com",
+    projectId: "portfolio-e0307",
+    storageBucket: "portfolio-e0307.appspot.com",
+    messagingSenderId: "859018356870",
+    appId: "1:859018356870:web:536bccf0be08f1068c4907",
+    measurementId: "G-DPWW7FNMCT"
+  }
 };
