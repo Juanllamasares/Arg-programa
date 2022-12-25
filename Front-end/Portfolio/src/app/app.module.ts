90e282fefb73886542818a5cea5bbad37c8c8a05
Juan <juanllama160@gmail.com>
2022-12-25 10:36:17 -0300

    modificando host back y front

--- Front-end/Portfolio/src/app/app.module.ts
+++ Front-end/Portfolio/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [
     AppComponent,
